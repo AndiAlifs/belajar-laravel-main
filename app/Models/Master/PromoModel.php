586feabc8a72b234401f0c0bf94bcbb8463c845a
AndiAlifs <andyalyfsyah4@gmail.com>
2022-12-19 10:34:24 +0700

    make model promo

--- app/Models/Master/PromoModel.php
+++ app/Models/Master/PromoModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Api\Master;
+namespace App\Models\Master;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class PromoModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'm_promo';
+
+    protected $guarded = [];
+
+    public function getVoucherPromo()
+    {
+        $promo = $this->where('type', 'voucher')->get();
+        return $promo;
+    }
+
+    public function getDiscountPromo()
+    {
+        $promo = $this->where('type', 'diskon')->get();
+        return $promo;
+    }
 }

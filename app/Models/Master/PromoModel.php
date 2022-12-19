e5f26cce0da7d8ed35342a780592d01bdbe8a13a
AndiAlifs <andyalyfsyah4@gmail.com>
2022-12-19 10:27:14 +0700

    create controller and model for promo

---
+++ app/Models/Master/PromoModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Api\Master;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PromoModel extends Model
+{
+    use HasFactory;
+}
